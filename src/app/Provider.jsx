67240d754079a4e0db31095be4bdc871841765de
Habiba Hassan <habiba.hassaneen@gmail.com>
2024-12-28 19:55:09 +0200

    add chart

--- src/app/Provider.jsx
+++ src/app/Provider.jsx
@@ -1,10 +1,13 @@
 'use client'
 
 import {NextUIProvider} from '@nextui-org/react'
+import {ThemeProvider as NextThemesProvider} from "next-themes";
 export default function Provider(  {children}) {
   return (
+
     <NextUIProvider>
     {children}
   </NextUIProvider>
+
   )
 }
